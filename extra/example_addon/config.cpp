9673bcc1ca6e7deb651678eff25afa987959f606
DartRuffian <skulldude327@gmail.com>
2024-08-19 10:05:47 -0500

    Added url to CfgPatches

--- extra/example_addon/config.cpp
+++ extra/example_addon/config.cpp
@@ -4,6 +4,7 @@ class CfgPatches {
     class ADDON {
         author = AUTHOR;
         authors[] = {"You!"};
+        url = ECSTRING(main,url);
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
