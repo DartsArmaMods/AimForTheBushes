5540b4d6ff5d713cfbb995b644dc9acb945e5af2
DartRuffian <skulldude327@gmail.com>
2024-08-27 12:08:17 -0500

    Hotfix 1.0.1
    Fixed version number.

--- mod.cpp
+++ mod.cpp
@@ -3,6 +3,6 @@ name = "Aim For The Bushes";
 description = "Aim For The Bushes - Version 0.0.0";
 overview = "";
 overviewPicture = "logo_aftb_ca.paa";
-picture = "logo_aftb.png";
+picture = "logo_aftb_ca.paa";
 logo = "logo_aftb_ca.paa";
 logoOver = "logo_aftb_ca.paa";
