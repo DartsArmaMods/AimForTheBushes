cdbc17b36d9b09d3ef4881a423a8597ff2421f40
DartRuffian <skulldude327@gmail.com>
2024-08-27 12:27:56 -0500

    Update

--- addons/compat_ls/CfgVehicles.hpp
+++ addons/compat_ls/CfgVehicles.hpp
@@ -1,12 +1,12 @@
 class CfgVehicles {
     class Helicopter_Base_H;
-    class lsd_laat_base: Helicopter_Base_H {
+    class ls_laati_base: Helicopter_Base_H {
         GVARMAIN(rampAnims)[] = {{"laat_ramp_open", 0, 1}};
 
         EGVAR(helocast,boatPositions)[] = {{0, -3, 0.2}};
         EGVAR(helocast,marker) = "Chemlight_green";
     };
-    class lsd_heli_laati_ab: lsd_laat_base {
+    class ls_laati_airborne: ls_laati_base {
         EGVAR(staticLine,enabled) = 1;
         EGVAR(staticLine,condition) = QUOTE(true);
     };
--- addons/compat_ls/staticline/CfgVehicles.hpp
+++
@@ -1,7 +0,0 @@
-class CfgVehicles {
-    class ls_laati_base;
-    class ls_laati_airborne: ls_laati_base {
-        EGVAR(staticLine,enabled) = 1;
-        EGVAR(staticLine,condition) = QUOTE(true);
-    };
-};
