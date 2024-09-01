38517c9a858939e6ead585f4e99643c638d2af8d
DartRuffian <skulldude327@gmail.com>
2024-08-31 21:25:17 -0500

    3CB BAF Vehicles Compat

---
+++ addons/compat_uk3cb/CfgVehicles.hpp
@@ -0,0 +1,17 @@
+class CfgVehicles {
+    class Heli_Transport_02_base_F;
+    class UK3CB_BAF_Merlin_Base: Heli_Transport_02_base_F {
+        GVARMAIN(rampAnims)[] = {{"CargoRamp_Open", 0, 1}};
+        GVARMAIN(passengerTurrets)[] = {
+            {1}, {2}
+        };
+
+        EGVAR(staticLine,enabled) = 1;
+        EGVAR(staticLine,condition) = QUOTE(true);
+
+        EGVAR(helocast,boatPositions)[] = {
+            {0.25, 1.7, -0.8}
+        };
+        EGVAR(helocast,marker) = "Chemlight_green";
+    };
+};
