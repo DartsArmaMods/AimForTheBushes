9a9aad83a93195b8227c29aef1893280b85ea374
DartRuffian <skulldude327@gmail.com>
2024-10-05 21:05:48 -0500

    Added helocasting to SOG helicopters

--- addons/compat_sog/CfgVehicles.hpp
+++ addons/compat_sog/CfgVehicles.hpp
@@ -1,55 +1,5 @@
-#define STATICLINE EGVAR(staticLine,enabled) = 1; \
-EGVAR(staticLine,condition) = QUOTE(true)
-
 class CfgVehicles {
-    class vn_b_backpack_base;
-    class vn_b_pack_ba18_01: vn_b_backpack_base {
-        GVARMAIN(isParachute) = 1;
-    };
+    #include "cfg\parachutes.hpp"
 
-    class vn_o_backpack_base;
-    class vn_o_pack_parachute_01: vn_o_backpack_base {
-        GVARMAIN(isParachute) = 1;
-    };
-
-    class vn_air_ch47_04_base;
-    class vn_b_air_ch47_04_01: vn_air_ch47_04_base {
-        GVARMAIN(passengerTurrets)[] = {
-            {1}, {2}
-        };
-        GVARMAIN(rampAnims)[] = {{"ramp", 0, 1}};
-
-        EGVAR(staticLine,enabled) = 1;
-        EGVAR(staticLine,condition) = QUOTE(true);
-    };
-    class vn_b_air_ch47_04_02: vn_air_ch47_04_base {
-        GVARMAIN(passengerTurrets)[] = {
-            {1}, {2}
-        };
-        GVARMAIN(rampAnims)[] = {{"ramp", 0, 1}};
-
-        EGVAR(staticLine,enabled) = 1;
-        EGVAR(staticLine,condition) = QUOTE(true);
-    };
-
-    class vn_air_ch47_01_base;
-    class vn_b_air_ch47_01_01: vn_air_ch47_01_base {
-        GVARMAIN(passengerTurrets)[] = {
-            {1}, {2}
-        };
-        GVARMAIN(rampAnims)[] = {{"ramp", 0, 1}};
-
-        EGVAR(staticLine,enabled) = 1;
-        EGVAR(staticLine,condition) = QUOTE(true);
-    };
-
-    class vn_b_air_ch47_01_02: vn_air_ch47_01_base {
-        GVARMAIN(passengerTurrets)[] = {
-            {1}, {2}
-        };
-        GVARMAIN(rampAnims)[] = {{"ramp", 0, 1}};
-
-        EGVAR(staticLine,enabled) = 1;
-        EGVAR(staticLine,condition) = QUOTE(true);
-    };
+    #include "cfg\helicopters.hpp"
 };
--- extra/example_subaddon/script_component.hpp
+++ extra/example_subaddon/script_component.hpp
@@ -1,4 +1,4 @@
-#define SUBCOMPONENT subaddonName
+#define SUBCOMPONENT subaddonname
 #define SUBCOMPONENT_BEAUTIFIED Sub Addon
 
 #include "..\script_component.hpp"
