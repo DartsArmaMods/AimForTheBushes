a4fc326adacef2928b3cc9b3a3044622c6542b2b
Nightwolf2113 <120837308+Nightwolf2113@users.noreply.github.com>
2024-10-11 15:14:29 -0700

    Add files via upload

---
+++ addons/compat_3as/script_component.hpp
@@ -0,0 +1,9 @@
+#define COMPONENT compat_3as
+#define COMPONENT_BEAUTIFIED 3AS Compatability
+#include "\z\aftb\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#include "\z\aftb\addons\main\script_macros.hpp"
