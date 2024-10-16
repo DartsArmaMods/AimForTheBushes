63db02dc9363f4d5c66b38f6e972a7f1a143a524
clbt404 <76954722+clbt404@users.noreply.github.com>
2024-10-17 01:15:23 +0300

    Update CfgVehicles.hpp
    Add static line compatibility for GM west and east planes

--- addons/compat_gm/CfgVehicles.hpp
+++ addons/compat_gm/CfgVehicles.hpp
@@ -1,2 +1,16 @@
 class CfgVehicles {
+	class gm_plane_base;
+	class gm_do28d2_base : gm_plane_base {
+		aftb_staticLine_enabled = 1;
+        aftb_staticLine_condition = "true";
+	};
+	class gm_do28d2_medevac_base : gm_do28d2_base {
+		aftb_staticLine_enabled = 0;
+        aftb_staticLine_condition = "true";
+	};
+	class gm_l410_base;
+	class gm_l410t_base : gm_l410_base {
+		aftb_staticLine_enabled = 1;
+        aftb_staticLine_condition = "true";
+	};
 };
