7fc33af13f461ee0d5c0093495d8201b839dbc3b
clbt404 <76954722+clbt404@users.noreply.github.com>
2024-10-18 00:17:29 +0300

    Update CfgVehicles.hpp

--- addons/compat_gm/CfgVehicles.hpp
+++ addons/compat_gm/CfgVehicles.hpp
@@ -1,6 +1,9 @@
 class CfgVehicles {
     class gm_plane_base;
     class gm_do28d2_base: gm_plane_base {
+        aftb_rampAnims[] = {
+            {["", 0, 0]}
+        };
         aftb_staticLine_enabled = 1;
         aftb_staticLine_condition = "true";
     };
@@ -10,6 +13,9 @@ class CfgVehicles {
     };
     class gm_l410_base;
     class gm_l410t_base: gm_l410_base {
+        aftb_rampAnims[] = {
+            {["", 0, 0]}
+        };
         aftb_staticLine_enabled = 1;
         aftb_staticLine_condition = "true";
     };
