5e213593a601726c88be4ab172af3ff2932343c6
clbt404 <76954722+clbt404@users.noreply.github.com>
2024-10-20 14:31:59 +0300

    Update addons/compat_gm/CfgVehicles.hpp
    Co-authored-by: Dart <59131299+DartRuffian@users.noreply.github.com>

--- addons/compat_gm/CfgVehicles.hpp
+++ addons/compat_gm/CfgVehicles.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
         GVARMAIN(rampAnims)[] = {
             {"", 0, 0}
         };
-        aftb_staticLine_enabled = 1;
+        EGVAR(staticLine,enabled) = 1;
         aftb_staticLine_condition = "true";
     };
     class gm_do28d2_medevac_base: gm_do28d2_base {
