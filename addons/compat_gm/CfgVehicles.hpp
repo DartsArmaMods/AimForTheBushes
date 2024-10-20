725dd7907fb44566bb1f3e6cad6c3ece45d9af95
clbt404 <76954722+clbt404@users.noreply.github.com>
2024-10-20 14:33:26 +0300

    Update CfgVehicles.hpp

--- addons/compat_gm/CfgVehicles.hpp
+++ addons/compat_gm/CfgVehicles.hpp
@@ -5,18 +5,18 @@ class CfgVehicles {
             {"", 0, 0}
         };
         EGVAR(staticLine,enabled) = 1;
-        aftb_staticLine_condition = "true";
+        EGVAR(staticLine,condition) = "true";
     };
     class gm_do28d2_medevac_base: gm_do28d2_base {
-        aftb_staticLine_enabled = 0;
-        aftb_staticLine_condition = "true";
+        EGVAR(staticLine,enabled) = 0;
+        EGVAR(staticLine,condition) = "true";
     };
     class gm_l410_base;
     class gm_l410t_base: gm_l410_base {
-        aftb_rampAnims[] = {
+        GVARMAIN(rampAnims)[] = {
             {"", 0, 0}
         };
-        aftb_staticLine_enabled = 1;
-        aftb_staticLine_condition = "true";
+        EGVAR(staticLine,enabled) = 1;
+        EGVAR(staticLine,condition) = "true";
     };
 };
