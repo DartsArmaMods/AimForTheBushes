396c35d67d846683b4fa51be46d005d29251adb0
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2025-02-16 14:04:32 +0100

    Fixup component authors
    Co-authored-by: Dart <59131299+DartRuffian@users.noreply.github.com>

--- addons/compat_3cbfactions/config.cpp
+++ addons/compat_3cbfactions/config.cpp
@@ -3,7 +3,7 @@
 class CfgPatches {
     class ADDON {
         author = AUTHOR;
-        authors[] = {"DartRuffian", "mrschick"};
+        authors[] = {"mrschick"};
         url = ECSTRING(main,url);
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
